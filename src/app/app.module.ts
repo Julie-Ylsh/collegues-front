d53e7c218eb536d2ee5e0f27095224834a8430ab
julie <julie.bouaye@hotmail.fr>
2019-04-26 12:41:38 +0200

    Carroussel d'images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, NgbModule
+    BrowserModule, NgbModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
