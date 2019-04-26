5538f74998f41263f55d25a3418981eab50e4be8
julie <julie.bouaye@hotmail.fr>
2019-04-26 11:48:01 +0200

    Ajout bootstrap et ng bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+
 if (environment.production) {
   enableProdMode();
 }
