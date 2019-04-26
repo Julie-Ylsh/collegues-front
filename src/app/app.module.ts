ebf58ba924d7cb25117e000f79a44f4935b7a273
julie <julie.bouaye@hotmail.fr>
2019-04-26 17:33:55 +0200

    Version stable avec modal (sans formulaire)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModule,NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CollegueComponent } from './collegue/collegue.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CollegueComponent
-  ],
+      ],
   imports: [
     BrowserModule, NgbModule, ReactiveFormsModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule { 
+  
+}
+
+
--- src/app/collegue/collegue.component.ts
+++ src/app/collegue/collegue.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Collegue } from '../models/Collegue';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+
 
 @Component({
   selector: 'app-collegue',
@@ -8,12 +11,32 @@ import { Collegue } from '../models/Collegue';
 })
 export class CollegueComponent implements OnInit {
   @Input() col: Collegue;
-  constructor() { }
 
   ngOnInit() {
   }
 
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) { }
+
+  open(content) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Annulé ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `avec : ${reason}`;
+    }
+  }
 }
 
 
-  
+
